8d744bcf0d50ce931cc4f53fbe21ff5c4bb34e4d
Sergei <legal.666@mail.ru>
2022-09-12 17:41:42 +0300

    первая фиксация

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -10,19 +10,19 @@ int y2 = Coordinate("y", "B");
 int z2 = Coordinate("z", "B");
 
 int Coordinate(string coorName, string pointName)
-           {
-            Console.Write($"Введите координату {coorName} точки {pointName}: ");
-            return Convert.ToInt16(Console.ReadLine());
-           }
+       {
+        Console.Write($"Введите координату {coorName} точки {pointName}: ");
+        return Convert.ToInt16(Console.ReadLine());
+       }
 
 double Decision(double x1, double x2, 
                 double y1, double y2, 
                 double z1, double z2)
-           {
-            return Math.Sqrt(Math.Pow((x2-x1), 2) + 
-                   Math.Pow((y2-y1), 2) + 
-                   Math.Pow((z2-z1), 2));
-           }
+       {
+        return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+        Math.Pow((y2-y1), 2) + 
+        Math.Pow((z2-z1), 2));
+       }
 
 double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
 
